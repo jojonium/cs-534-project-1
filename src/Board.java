37abf285e89e77ccfa828a60e3a28a943fb4bb9f
jaturcotte <jaturcotte@wpi.edu>
2020-02-12 16:15:19 -0500

    Fixed hill climbing

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,6 @@
 // Board class for Heavy N-Queens
 // CS534 Assignment 1
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -105,77 +104,78 @@ public class Board {
         return positions;
     }
 
-    /**
-     * This method calculates the simple heuristic for the board (# queens attacking in/directly).
-     * @param positions (the positions of the queens)
-     */
-    public int h(int[] positions) {
-        int h = 0;
-        int N = this.getN();
-
-        // calculate how many queens we are attacking row- and diagonal-wise and add to h
-        for(int i = 0; i < N; i++) {
-            int[] row_attack = this.attackRow(positions[i], i);
-            h += IntStream.of(row_attack).sum();
-            int[] diag_attack = this.attackDiag(positions[i], i);
-            h += IntStream.of(diag_attack).sum();
-        }
-
-        // divide by 2 since we double-count above
-        return (h / 2);
-    }
+//    /**
+//     * This method calculates the simple heuristic for the board (# queens attacking in/directly).
+//     * @param positions (the positions of the queens)
+//     */
+//    public int h(int[] positions) {
+//        int h = 0;
+//        int N = this.getN();
+//
+//        // calculate how many queens we are attacking row- and diagonal-wise and add to h
+//        for(int i = 0; i < N; i++) {
+//            int[] row_attack = this.attackRow(positions[i], i, positions);
+//            h += IntStream.of(row_attack).sum();
+//            int[] diag_attack = this.attackDiag(positions[i], i, positions);
+//            h += IntStream.of(diag_attack).sum();
+//        }
+//
+//        // divide by 2 since we double-count above
+//        return (h / 2);
+//    }
 
     /**
      * This method calculates the first heuristic for the board.
-     * @return lightest_indices (indices of lightest queens attacking in/directly)
+     * @return h (the weight of the lightest queen, squared)
      */
-    public ArrayList<Integer> h1() {
-        int[] queen_positions = this.getQueenPositions();
+    public int h1(int[] positions) {
         int[] queen_weights = this.getWeights();
         int N = this.getN();
-        ArrayList<Integer> lightest_indices = new ArrayList<>();
-        int lightest_index; // keeps track of where the lightest queen is located
-        int lightest_value = 10; // always heavier than heaviest queen
+        int lightest_index = positions[0]; // keeps track of where the lightest queen is located
+        int lightest_queen = 10; // always heavier than heaviest queen
+        boolean any_attacking = false; // tracks if any queens are attacking
 
         for(int i = 0; i < N; i++) {
             // initialize how many queens we are attacking
             int attacking = 0;
 
             // calculate the # of queens this queen is attacking (in)directly
-            int[] row_attack = this.attackRow(queen_positions[i], i);
+            int[] row_attack = this.attackRow(positions[i], i, positions);
             attacking += IntStream.of(row_attack).sum();
-            int[] diag_attack = this.attackDiag(queen_positions[i], i);
+            int[] diag_attack = this.attackDiag(positions[i], i, positions);
             attacking += IntStream.of(diag_attack).sum();
 
             // update if we find a lighter attacking queen
-            if (attacking > 0 && queen_weights[i] < lightest_value) {
-                lightest_indices.clear();
+            if (attacking > 0 && queen_weights[i] < lightest_queen) {
+                any_attacking = true;
                 lightest_index = i;
-                lightest_value = queen_weights[lightest_index];
-                lightest_indices.add(i);
-            } else if (attacking > 0 && queen_weights[i] == lightest_value) {
-                lightest_indices.add(i);
+                lightest_queen = queen_weights[lightest_index];
             }
         }
 
-        System.out.println("The weight of the lightest queen(s) attacking is " + lightest_value);
-        System.out.println("The value of h1 is: " + (int)(Math.pow(lightest_value, 2)));
+//        System.out.println("The weight of the lightest queen(s) attacking is " + lightest_queen);
+//        System.out.println("The value of h1 is: " + (int)(Math.pow(lightest_queen, 2)));
 
-        return lightest_indices;
+        // return h1 if any queens are attacking, 0 otherwise
+        int h1 = (int)Math.pow(queen_weights[lightest_index], 2);
+        if (any_attacking) {
+            return h1;
+        } else {
+            return 0;
+        }
     }
 
     /**
      * This method calculates the second heuristic for the board (sum of lightest queens attacking in/directly).
-     * @return hl_indices (the indices of the heaviest queens that are lighter in all their pairs)
+     * @return hl_value (the sum of the weights of the lighter queen for each pair)
      */
-    public ArrayList<Integer> h2() {
-        int[] queen_positions = this.getQueenPositions();
+    public int h2(int[] positions) {
         int[] queen_weights = this.getWeights();
         int N = this.getN();
         int h2 = 0;
         int hl_value = 0; // lightest queen will always be bigger
         int hl_index;
-        ArrayList<Integer> hl_indices = new ArrayList<>();
+        boolean any_attacking = false; // tracks if any queens are attacking
 
         for(int i = 0; i < N; i++) {
             // initialize how many queens we are attacking
@@ -185,15 +185,19 @@ public class Board {
             boolean always_lighter = true;
 
             // calculate the # of queens this queen is row attacking (in)directly
-            int[] row_attack = this.attackRow(queen_positions[i], i);
+            int[] row_attack = this.attackRow(positions[i], i, positions);
             attacking += IntStream.of(row_attack).sum();
 
             // calculate the # of queens this queen is diagonal attacking (in)directly
-            int[] diag_attack = this.attackDiag(queen_positions[i], i);
+            int[] diag_attack = this.attackDiag(positions[i], i, positions);
             attacking += IntStream.of(diag_attack).sum();
 
             // consider each queen we attacked
             if (attacking > 0) {
+
+                // update attacking flag
+                any_attacking = true;
+
                 for(int j = 0; j < N; j++) {
                     // compare this queen to each of the ones it is attacking
                     if (row_attack[j] == 1) {
@@ -217,21 +221,22 @@ public class Board {
 
                 // update (or add to) HL queens list based on the "heaviest-lightest" rule
                 if (queen_weights[i] > hl_value && always_lighter) {
-                    hl_indices.clear();
                     hl_index = i;
                     hl_value = queen_weights[hl_index];
-                    hl_indices.add(i);
-                } else if (queen_weights[i] == hl_value && always_lighter) {
-                    hl_indices.add(i);
                 }
             }
         }
 
         // divide by 2 since we double-count above
-        System.out.println("The weight of the heaviest-lightest queen(s) attacking is " + hl_value);
-        System.out.println("The value of h2 is: " + (h2 / 2));
+//        System.out.println("The weight of the heaviest-lightest queen(s) attacking is " + hl_value);
+//        System.out.println("The value of h2 is: " + (h2 / 2));
 
-        return hl_indices;
+        // return h2 if any queens are attacking, 0 otherwise
+        if (any_attacking) {
+            return (h2 / 2);
+        } else {
+            return 0;
+        }
     }
 
     /**
@@ -242,43 +247,47 @@ public class Board {
 
         int[] queen_positions = this.getQueenPositions();
         int N = this.getN();
-        ArrayList<Integer> candidates; // keeps track of candidate queens for movement
+        int lowest_h; // keeps track of lowest h value
 
         while(true) {
             // calculate heuristic of the board based on which one we are using
             if (h_to_use == 1) {
-                candidates = this.h1();
+                lowest_h = this.h1(queen_positions);
             } else {
-                candidates = this.h2();
+                lowest_h = this.h2(queen_positions);
             }
-            System.out.println("Candidates for movement: " + candidates.toString());
 
-            // we win if the candidate list is empty
-            if(candidates.size() == 0) break;
+            // we win if no queens are attacking
+            if(lowest_h == 0) break;
 
             // keep track of whether we improved
             boolean improvement = false;
 
             // check possible moves we can make
-            int current_queen = candidates.get(0);
-            int h = this.h(queen_positions);
-            int h_index = queen_positions[current_queen];
-            for (Integer index : candidates) {
+            int current_queen = queen_positions[0];
+            int h_index = queen_positions[0];
+            for (int index = 0; index < N; index++) {
 
                 // create a clone of the queen positions for each new piece
                 int[] temp_positions = new int[this.getQueenPositions().length];
                 System.arraycopy(this.getQueenPositions(), 0, temp_positions, 0, this.getQueenPositions().length);
 
-                // for each minimum queen, consider each of its moves
+                // for each queen, consider each of its moves
                 for (int j = 0; j < N; j++) {
                     temp_positions = this.move_queen(index, j, temp_positions);
 
-                    // move the queen that produces the lowest h value
-                    int new_h = this.h(temp_positions);
-                    if (new_h < h) {
-                        h = new_h;
-                        h_index = j;
+                    // move the queen that produces the lowest heuristic value
+                    int new_h;
+                    if (h_to_use == 1) {
+                        new_h = this.h1(temp_positions);
+                    } else {
+                        new_h = this.h2(temp_positions);
+                    }
+
+                    if (new_h < lowest_h) {
+                        lowest_h = new_h;
                         current_queen = index;
+                        h_index = j;
                         improvement = true;
                     }
                 }
@@ -303,15 +312,14 @@ public class Board {
      *        col (the column the queen is located in)
      * @return attacking (the positions of the queens being attacked)
      */
-    private int[] attackRow(int row, int col) {
+    private int[] attackRow(int row, int col, int[] positions) {
 
-        int[] queen_positions = this.getQueenPositions();
         int N = this.getN();
         int[] attacking = new int[N];
 
         for(int i = 0; i < N; i++) {
             // check if the row is the same (and not self-attacking)
-            if(i != col && queen_positions[i] == row) {
+            if(i != col && positions[i] == row) {
                 attacking[i] = 1;
             } else {
                 attacking[i] = 0;
@@ -327,15 +335,14 @@ public class Board {
      *        col (the column the queen is located in)
      * @return attacking (the positions of the queens being attacked)
      */
-    private int[] attackDiag(int row, int col) {
+    private int[] attackDiag(int row, int col, int[] positions) {
 
-        int[] queen_positions = this.getQueenPositions();
         int N = this.getN();
         int[] attacking = new int[N];
 
         for(int i = 0; i < N; i++) {
             // calculate if the row and column difference are the same (and not self-attacking)
-            if( (i != col) && (Math.abs(queen_positions[i] - row) == Math.abs(i - col)) ) {
+            if( (i != col) && (Math.abs(positions[i] - row) == Math.abs(i - col)) ) {
                 attacking[i] = 1;
             } else {
                 attacking[i] = 0;
@@ -365,8 +372,8 @@ public class Board {
             b.generateRandomBoard(N);
         } else {
             // read queen positions and weights from file (hardcoded for now)
-            int[] queenPositions = new int[]{0, 3, 4, 1, 0};
-            int[] queenWeights = new int[]{4, 6, 3, 4, 7};
+            int[] queenPositions = new int[]{4, 2, 4, 1, 3};
+            int[] queenWeights = new int[]{9, 1, 1, 1, 1};
             b.generateBoardFromFile(queenPositions, queenWeights);
         }
 
