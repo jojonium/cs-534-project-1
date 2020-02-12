eed192d01aaec3d92aa6bd1e3465e398037a18f8
Joseph Petitti <jppetitti@wpi.edu>
2020-02-12 17:06:26 -0500

    usage formatting fix

--- src/Main.java
+++ src/Main.java
@@ -119,8 +119,9 @@ public class Main {
 	 */
 	private static void showUsage() {
 		System.out.println("Usage:");
-		System.out.println("\t./nqueens FILENAME ALGORITHM HEURISTIC\n");
+		System.out.println("\t./nqueens FILENAME ALGORITHM HEURISTIC");
 		System.out.println("\t./nqueens SIZE ALGORITHM HEURISTIC\n");
+		System.out.println("FILENAME: path to CSV board file");
 		System.out.println("SIZE: number of queens");
 		System.out.println("ALGORITHM: 1 for A*, 2 for greedy hill climbing");
 		System.out.println("HEURISTIC: H1 or H2");
